4e1de3ee1843cbeada3861e8a00faa42b080bfb5
Norman Moon <norman.moon@va.gov>
2024-09-19 12:01:42 -0400

    making tickets not in sqa and sqa-beta

--- create-parent-ticket-test.sh
+++ create-parent-ticket-test.sh
@@ -38,36 +38,36 @@ done
 
 
 
+if [[ "${environment,,}" != "sqa" ]] && [[ "${environment,,}" != "sqa-beta" ]]; then
+     template='{
 
-template='{
+       "fields": {
+         "summary": "%s",
+         "project": {
+           "id": "%s"
+         },
+         "issuetype": {
+           "id": "%s"
+         },
+         "description": "%s"
 
-  "fields": {
-    "summary": "%s",
-    "project": {
-      "id": "%s"
-    },
-    "issuetype": {
-      "id": "%s"
-    },
-    "description": "%s"
+         }
+     }'
 
-    }
-}'
+     json_final=$(printf "$template" \
+                         "$summary" \
+                         "$project_id" \
+                         "$issuetype_id" \
+                         "$description")
 
-json_final=$(printf "$template" \
-                    "$summary" \
-                    "$project_id" \
-                    "$issuetype_id" \
-                    "$description")
-
-curl -v -i -X POST \
-          -u norman.moon@aboutobjects.com:"$token" \
-          -H "Content-Type:application/json" \
-          -H "Accept: application/json" \
-          -H "X-Atlassian-Token:no-check" \
-          "https://normanmoon.atlassian.net/rest/api/2/issue/" \
-          -d \
-          "$json_final" \
-          -o create-parent-ticket-test.out
-
+     curl -v -i -X POST \
+               -u norman.moon@aboutobjects.com:"$token" \
+               -H "Content-Type:application/json" \
+               -H "Accept: application/json" \
+               -H "X-Atlassian-Token:no-check" \
+               "https://normanmoon.atlassian.net/rest/api/2/issue/" \
+               -d \
+               "$json_final" \
+               -o create-parent-ticket-test.out
+fi
 cat create-parent-ticket-test.out
