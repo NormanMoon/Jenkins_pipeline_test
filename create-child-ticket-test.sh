d7593812766ce3bbf72fac8f70437df71564368d
Norman Moon <norman.moon@va.gov>
2024-10-16 16:13:03 -0400

    changed the array being referenced

--- create-child-ticket-test.sh
+++ create-child-ticket-test.sh
@@ -40,7 +40,7 @@ echo "services after cleaning: ${services[*]}"
 for ((j=0; j<${#services[@]}; j++)) do
      echo "Service Name : ${services[j],,}"
 
-     if [[ "${services[j],,}" = "deployment" ]] || [[ "${services[j],,}" = "main" ]] || [[ "${services[j],,}" = "HFD" ]] || [[ "${services[j],,}" = "arch" ]]; then
+     if [[ "${services[j],,}" = "deployment" ]] || [[ "${services[j],,}" = "main" ]] || [[ "${services[j],,}" = "hfd" ]] || [[ "${services[j],,}" = "arch" ]]; then
           issuetype_id+=("10011")
      else
           issuetype_id+=("10008")
