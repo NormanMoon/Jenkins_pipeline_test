0e59609f630a328f93a2d2271d939888f37a6cd0
Norman Moon <norman.moon@va.gov>
2024-12-18 12:33:17 -0500

    stuff

--- create_ticket_descriptions_functional.sh
+++ create_ticket_descriptions_functional.sh
@@ -33,38 +33,17 @@ main() {
      project_id=$(get_project_id)
      # Clean services and retrieve necessary data
      local services=($(clean_all_services "$application" "${input_services[@]}"))
-     local image=$(get_image "$application")
+
      local child_tickets=($(return_child_ticket_list "$application" "${services[@]}"))
-     local summaries=($(create_ticket_summary_list "$env" "$image" "$app_version" "$application" "${services[@]}"))
+
 
      # Initialize description array
      local descriptions=()
-     local next_step_symbol
-     next_step_symbol=$(get_next_step_symbol)
 
-     # Use map to create descriptions
-     for ((i=0; i<${#child_tickets[@]}; i++)); do
-       local is_vault_ticket="false"
 
-       # Create the new description
-       descriptions[i]=$(create_ticket_description "$is_vault_ticket" "$vault_description" "$image" "$app_version" "${services[i]}")
+     # shellcheck disable=SC2207
+     descriptions=($(generate_all_descriptions "$vault_description" "$image" "$app_version" "${services[@]}"))
 
-       # Remove previous "next step" symbol if necessary
-       if ((i > 0)); then
-           descriptions[i]=$(remove_previous_next_step "${descriptions[i]}")
-       fi
-
-       # Add "next step" symbol to the next description
-       if ((i < ${#child_tickets[@]} - 1)); then
-           descriptions[i+1]=$(add_next_step_to_description "${descriptions[i+1]}")
-       fi
-     done
-
-     # Escape newlines for JSON
-     local json_descriptions=()
-     for desc in "${descriptions[@]}"; do
-       json_descriptions+=("$(echo "$desc" | sed ':a;N;$!ba;s/\n/\\n/g')")
-     done
 
      # Output for debugging
      echo "Child tickets: ${child_tickets[*]}"
--- lib/utils.sh
+++ lib/utils.sh
@@ -272,6 +272,49 @@ create_ticket_description() {
     fi
 }
 
+generate_description() {
+    local index=$1
+    local child_tickets_count=$2
+    local vault_description=$3
+    local image=$4
+    local app_version=$5
+    local service=$6
+    local is_vault_ticket="false"
+
+    # Create the base description
+    local description
+    description=$(create_ticket_description "$is_vault_ticket" "$vault_description" "$image" "$app_version" "$service")
+
+    # Remove previous "next step" symbol if necessary
+    if ((index > 0)); then
+        description=$(remove_previous_next_step "$description")
+    fi
+
+    # Add "next step" symbol to the next description if it's not the last one
+    if ((index < child_tickets_count - 1)); then
+        description=$(add_next_step_to_description "$description")
+    fi
+
+    echo "$description"
+}
+
+generate_all_descriptions() {
+    local vault_description=$1
+    local image=$2
+    local app_version=$3
+    shift 3
+    local services=("$@")
+    local descriptions=()
+    local child_tickets_count=${#services[@]}
+
+    for ((i = 0; i < child_tickets_count; i++)); do
+        descriptions+=("$(generate_description "$i" "$child_tickets_count" "$vault_description" "$image" "$app_version" "${services[i]}")")
+    done
+
+    echo "${descriptions[@]}"
+}
+
+
 # Remove "next step" indicator from a description
 remove_previous_next_step() {
     local description=$1
@@ -287,8 +330,5 @@ add_next_step_to_description() {
     next_step_symbol=$(get_next_step_symbol)
     echo "${description} ${next_step_symbol}"
 }
-
-
-
 
 
